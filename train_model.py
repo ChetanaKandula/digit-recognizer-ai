98e57ace0eab9fb52bf018821ec34807e86d231d
Chetana <chetanachowdary12@gmail.com>
2026-03-16 08:12:06 +0000

    train

---
+++ train_model.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import matplotlib.pyplot as plt
+
+# Load dataset
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+
+# Normalize the data
+x_train = x_train / 255.0
+x_test = x_test / 255.0
+
+# Build CNN model
+model = keras.Sequential([
+    layers.Reshape((28,28,1), input_shape=(28,28)),
+    layers.Conv2D(32, (3,3), activation='relu'),
+    layers.MaxPooling2D((2,2)),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(10, activation='softmax')
+])
+
+# Compile model
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+# Train model
+model.fit(x_train, y_train, epochs=5)
+
+# Evaluate model
+test_loss, test_acc = model.evaluate(x_test, y_test)
+
+print("Test accuracy:", test_acc)
+
+# Save model
+model.save("digit_model.h5")
